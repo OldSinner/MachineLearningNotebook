83a2ff872dcd0a4bd01a30c61fb8ffd1a9aaf79f
OldSinner <antylifepatia@gmail.com>
2022-10-06 22:30:34 +0200

    init sketch

---
+++ Pereptron/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(1);
+}
+function draw() {
+    background(0, 20);
+    circle(mouseX, mouseY, 50);
+}
---
+++ Pereptron/sketch/sketch.ts
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  background(1)
+}
+
+function draw() {
+  background(0, 20)
+  circle(mouseX, mouseY, 50)
+}
