5fd9aa8ff97f7b25ad8a2bf694fd1d8f3a499323
OldSinner <antylifepatia@gmail.com>
2022-10-18 00:36:20 +0200

    direcotry of project changes

---
+++ src/Sketchs/MultiLayerPerceptron/main.ts
@@ -0,0 +1,6 @@
+import MatrixMath from '../../Math/MatrixMath.js'
+
+var matrix = new MatrixMath(3, 3)
+matrix.ScalarAdd(5)
+matrix.ScalarMultiply(2)
+console.log(matrix)
--- src/console.ts
+++ src/console.ts
@@ -1,6 +1,7 @@
-import MatrixMath from './MatrixMath.js'
+import MatrixMath from './Math/MatrixMath.js'
 
 var matrix = new MatrixMath(3, 3)
 matrix.ScalarAdd(5)
 matrix.ScalarMultiply(2)
 console.log(matrix)
+
