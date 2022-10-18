ef976c8964828e844660685483c57b3535452b8f
OldSinner <antylifepatia@gmail.com>
2022-10-18 23:03:11 +0200

    Martix/Matrix Summary

--- src/Modules/Math/MatrixMath.ts
+++ src/Modules/Math/MatrixMath.ts
@@ -2,6 +2,7 @@ export default class MatrixMath {
   rows: number
   cols: number;
   [n: number]: number[]
+  ///////////////////////////
   constructor(rows: number, cols: number) {
     this.rows = rows
     this.cols = cols
@@ -13,19 +14,38 @@ export default class MatrixMath {
       }
     }
   }
-
-  ScalarMultiply(scalar: number) {
+  ///////////////////////////
+  ScalarMultiply(scalar: number): MatrixMath {
     for (let i = 0; i < this.rows; i++) {
       for (let j = 0; j < this.cols; j++) {
         this[i][j] *= scalar
       }
     }
+    return this
   }
-  ScalarAdd(scalar: number) {
-    for (let i = 0; i < this.rows; i++) {
-      for (let j = 0; j < this.cols; j++) {
-        this[i][j] += scalar
+  ///////////////////////////
+  Add(scalar: number | MatrixMath): MatrixMath {
+    if (scalar instanceof MatrixMath) {
+      if (scalar.cols != this.cols || scalar.rows != this.rows) {
+        throw new Error(
+          'Columns and Rows of A must match Columns and Rows of B.',
+        )
       }
     }
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        this[i][j] += scalar instanceof MatrixMath ? scalar[i][j] : scalar
+      }
+    }
+    return this
+  }
+  ///////////////////////////
+  Randomize(): MatrixMath {
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        this[i][j] = Math.floor(Math.random() * 10)
+      }
+    }
+    return this
   }
 }
