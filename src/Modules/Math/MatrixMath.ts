d20b3b9f25b798e9b0269c542ec95112db32d973
OldSinner <antylifepatia@gmail.com>
2022-10-19 00:03:14 +0200

    Make Math Function static

--- src/Modules/Math/MatrixMath.ts
+++ src/Modules/Math/MatrixMath.ts
@@ -15,30 +15,69 @@ export default class MatrixMath {
     }
   }
   ///////////////////////////
-  ScalarMultiply(scalar: number): MatrixMath {
-    for (let i = 0; i < this.rows; i++) {
-      for (let j = 0; j < this.cols; j++) {
-        this[i][j] *= scalar
-      }
-    }
-    return this
-  }
-  ///////////////////////////
-  Add(scalar: number | MatrixMath): MatrixMath {
+  static Multiply(matrix: MatrixMath, scalar: number | MatrixMath): MatrixMath {
     if (scalar instanceof MatrixMath) {
-      if (scalar.cols != this.cols || scalar.rows != this.rows) {
+      if (matrix.cols != scalar.rows) {
+        throw new Error('Columns of A must match rows of B.')
+      }
+
+      var result = new MatrixMath(matrix.rows, scalar.cols)
+
+      for (let i = 0; i < result.rows; i++) {
+        for (let j = 0; j < result.cols; j++) {
+          let sum = 0
+          for (let k = 0; k < matrix.cols; k++) {
+            sum += matrix[i][k] * scalar[k][j]
+          }
+          result[i][j] = sum
+        }
+      }
+
+      return result
+    } else {
+      var newMatrix = matrix.clone()
+
+      for (let i = 0; i < newMatrix.rows; i++) {
+        for (let j = 0; j < newMatrix.cols; j++) {
+          newMatrix[i][j] *= scalar
+        }
+      }
+
+      return newMatrix
+    }
+  }
+
+  ///////////////////////////
+  static Add(matrix: MatrixMath, scalar: number | MatrixMath): MatrixMath {
+    var newMatrix = matrix.clone()
+
+    if (scalar instanceof MatrixMath) {
+      if (scalar.cols != newMatrix.cols || scalar.rows != newMatrix.rows) {
         throw new Error(
           'Columns and Rows of A must match Columns and Rows of B.',
         )
       }
     }
-    for (let i = 0; i < this.rows; i++) {
-      for (let j = 0; j < this.cols; j++) {
-        this[i][j] += scalar instanceof MatrixMath ? scalar[i][j] : scalar
+
+    for (let i = 0; i < newMatrix.rows; i++) {
+      for (let j = 0; j < newMatrix.cols; j++) {
+        newMatrix[i][j] += scalar instanceof MatrixMath ? scalar[i][j] : scalar
       }
     }
-    return this
+    return newMatrix
   }
+
+  ///////////////////////////
+  static Transpose(matrix: MatrixMath): MatrixMath {
+    var result = new MatrixMath(matrix.cols, matrix.rows)
+    for (let i = 0; i < matrix.rows; i++) {
+      for (let j = 0; j < matrix.cols; j++) {
+        result[j][i] = matrix[i][j]
+      }
+    }
+    return result
+  }
+
   ///////////////////////////
   Randomize(): MatrixMath {
     for (let i = 0; i < this.rows; i++) {
@@ -47,5 +86,9 @@ export default class MatrixMath {
       }
     }
     return this
+  }
+  ///////////////////////////
+  clone(): MatrixMath {
+    return JSON.parse(JSON.stringify(this))
   }
 }
--- src/console.ts
+++ src/console.ts
@@ -1,6 +1,11 @@
-import MatrixMath from './Math/MatrixMath.js'
+import MatrixMath from './Modules/Math/MatrixMath.js'
 
 var matrix = new MatrixMath(3, 3)
-matrix.ScalarAdd(5)
-matrix.ScalarMultiply(2)
-console.log(matrix[1])
+var matrix2 = new MatrixMath(3, 3)
+matrix.Randomize()
+matrix2.Randomize()
+console.table(MatrixMath.Add(matrix, matrix2))
+console.table(MatrixMath.Add(matrix, 5))
+console.table(MatrixMath.Multiply(matrix, matrix2))
+console.table(MatrixMath.Multiply(matrix, 5))
+console.table(MatrixMath.Transpose(matrix))
